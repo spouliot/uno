a4334a14a34792c12116573c99c8ba417f662632
Martin Zikmund <martin@zikmund.dev>
2022-02-14 16:03:48 +0100

    refactor: Skia BorderLayerRenderer

---
+++ src/Uno.UI/UI/Xaml/Controls/Border/BorderLayerRenderer.cs
@@ -0,0 +1,11 @@
+﻿namespace Windows.UI.Xaml.Shapes;
+
+internal partial class BorderLayerRenderer
+{
+	private readonly UIElement _owner;
+
+	public BorderLayerRenderer(UIElement owner)
+	{
+		_owner = owner;
+	}
+}
